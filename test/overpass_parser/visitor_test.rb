6f2186e18b3078612e7984b262887a29f2d1bd31
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2024-03-20 13:41:59 +0100

    Simplify tests

--- test/overpass_parser/visitor_test.rb
+++ test/overpass_parser/visitor_test.rb
@@ -6,33 +6,30 @@ class OverpassParser::TestVisitor < Minitest::Test
   def test_quote
     tree = OverpassParser.tree("node['highway'=\"primary\"];")
     assert_equal(
-      [{ type: :request,
-         queries: [{ type: :query_object, set: nil, object_type: "node",
-                     selectors: [{ type: :selector, value: "primary", key: "highway", operator: "=" }], filter: [] }] }], tree
+      { type: :query_object, set: nil, object_type: "node",
+        selectors: [{ type: :selector, value: "primary", key: "highway", operator: "=" }], filter: [] }, tree[0][:queries][0]
     )
   end
 
   def test_selector
     tree = OverpassParser.tree("node[highway=primary];")
     assert_equal(
-      [{ type: :request,
-         queries: [{ type: :query_object, set: nil, object_type: "node",
-                     selectors: [{ type: :selector, value: "primary", key: "highway", operator: "=" }], filter: [] }] }], tree
+      { type: :query_object, set: nil, object_type: "node",
+        selectors: [{ type: :selector, value: "primary", key: "highway", operator: "=" }], filter: [] }, tree[0][:queries][0]
     )
   end
 
   def test_selector_set
     tree = OverpassParser.tree("node._[highway=primary];")
     assert_equal(
-      [{ type: :request,
-         queries: [{ type: :query_object, set: "._", object_type: "node",
-                     selectors: [{ type: :selector, value: "primary", key: "highway", operator: "=" }], filter: [] }] }], tree
+      { type: :query_object, set: "._", object_type: "node",
+        selectors: [{ type: :selector, value: "primary", key: "highway", operator: "=" }], filter: [] }, tree[0][:queries][0]
     )
   end
 
   def test_recurse
     tree = OverpassParser.tree(">;")
-    assert_equal([{ type: :request, queries: [{ type: :query_recurse, recurse: ">" }] }], tree)
+    assert_equal({ type: :query_recurse, recurse: ">" }, tree[0][:queries][0])
   end
 
   def test_full
@@ -42,11 +39,10 @@ relation[name="En aban !"];
 nwr._[highway=bus_stop];
     ')
     assert_equal(
-      [{ type: :request,
-         queries: [
-           { type: :query_object, set: nil, object_type: "relation",
-             selectors: [{ type: :selector, value: "En aban !", key: "name", operator: "=" }], filter: [] }, { type: :query_recurse, recurse: ">" }, { type: :query_object, set: "._", object_type: "nwr", selectors: [{ type: :selector, value: "bus_stop", key: "highway", operator: "=" }], filter: [] }
-         ] }], tree
+      [
+        { type: :query_object, set: nil, object_type: "relation",
+          selectors: [{ type: :selector, value: "En aban !", key: "name", operator: "=" }], filter: [] }, { type: :query_recurse, recurse: ">" }, { type: :query_object, set: "._", object_type: "nwr", selectors: [{ type: :selector, value: "bus_stop", key: "highway", operator: "=" }], filter: [] }
+      ], tree[0][:queries]
     )
   end
 end
